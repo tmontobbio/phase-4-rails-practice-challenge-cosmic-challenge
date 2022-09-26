770c89df3b244cd1a96c36d2072d441e4bffa828
Tyler <toygrrr@gmail.com>
2022-09-26 08:29:38 -0700

    commit tree 80f1a9fdb22c745c13f5f3ca9c13dd253e5579e8

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,6 @@
 class Mission < ApplicationRecord
     belongs_to :scientist
     belongs_to :planet
+    validates :name, :scientist_id, :planet_id, presence: true
+    validates :scientist_id, uniqueness: true
 end
